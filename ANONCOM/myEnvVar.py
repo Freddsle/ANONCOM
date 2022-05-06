a11519b4da45800cdb88f5c6486d7eac7b2e0de8
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-05-06 20:13:06 +0200

    warning

--- ANONCOM/myEnvVar.py
+++ ANONCOM/myEnvVar.py
@@ -4,4 +4,4 @@ import os
 # Set environment variables
 def setVar():
 	os.environ['EMAIL_USER'] = 'bioinf.anoncom@gmail.com'
-	os.environ['EMAIL_PASS'] = 'bioinf2021'
+	os.environ['EMAIL_PASS'] = '--'
