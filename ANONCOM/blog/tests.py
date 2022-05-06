287072d4a80d9d2aae7628bd4bae5e509f562a3d
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-05-06 20:46:18 +0200

    add user posts redirecting

--- ANONCOM/blog/tests.py
+++ ANONCOM/blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class AllPostIndexViewTests(TestCase):
+
+    def test_no_post(self):
+        """
+        If no posts exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('blog-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "")
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title='just a test', content='a')
